af2407954e17e311123371345de5ba2bf1fe9d84
Adinarayan <adinarayanp2020@gmail.com>
2026-05-09 13:12:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+function App() {
+ const [count, setCount] = useState(0);
+ const [name,setName] = useState("");
+  return (
+    <>  
+    <h1>{count}</h1>  
+    <button onClick={() => setCount(count + 1)}>
+      +
+    </button>
+    <button onClick={()=> setCount(count-1)}>
+      -
+    </button>
+    </>
+  )
+}
+
+export default App
